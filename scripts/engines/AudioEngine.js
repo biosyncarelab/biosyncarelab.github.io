acd2b75706e9de7dc55d29976ae64b214e08d462
Renato Fabbri <renato.fabbri@gmail.com>
2025-11-21 00:07:58 +0100

    up

--- scripts/engines/AudioEngine.js
+++ scripts/engines/AudioEngine.js
@@ -34,16 +34,40 @@ export class AudioEngine {
   start() {
     if (this.isRunning) return;
     this.isRunning = true;
-    this._loop();
+
+    // Create a Blob worker to drive the loop even in background tabs
+    // This prevents audio modulation from freezing when the tab is hidden
+    const blob = new Blob([`
+      let interval = null;
+      self.onmessage = function(e) {
+        if (e.data === 'start') {
+          if (interval) clearInterval(interval);
+          interval = setInterval(() => {
+            self.postMessage('tick');
+          }, 33); // ~30fps
+        } else if (e.data === 'stop') {
+          if (interval) clearInterval(interval);
+          interval = null;
+        }
+      };
+    `], { type: 'application/javascript' });
+
+    this.worker = new Worker(URL.createObjectURL(blob));
+    this.worker.onmessage = () => this._tick();
+    this.worker.postMessage('start');
   }
 
   stop() {
     this.isRunning = false;
+    if (this.worker) {
+      this.worker.postMessage('stop');
+      this.worker.terminate();
+      this.worker = null;
+    }
   }
 
-  _loop() {
+  _tick() {
     if (!this.isRunning) return;
-    requestAnimationFrame(() => this._loop());
 
     // Use absolute time for parameter modulation to match MartigliOscillator
     let time;
@@ -284,114 +308,5 @@ export class AudioEngine {
       }
     }
   }
-  _initTrack(track) {
-    if (this.backend === 'webaudio') {
-      this._initWebAudioTrack(track);
-    }
-    // Future: Tone.js implementation
-  }
 
-  _stopTrack(track) {
-    const nodes = this.nodes.get(track.id);
-    if (nodes) {
-      try {
-        nodes.output.disconnect();
-        if (nodes.osc) nodes.osc.stop();
-      } catch (e) { /* ignore */ }
-      this.nodes.delete(track.id);
-    }
-  }
-
-  _initWebAudioTrack(track) {
-    // Basic implementation for Sine, Binaural, Isochronic
-    // This is a simplified factory. In a real app, we might have separate classes for these nodes.
-
-    const gain = this.ctx.createGain();
-    gain.connect(this.masterGain);
-
-    const nodes = { output: gain };
-
-    if (track.constructor.name === 'SineTrack') {
-      const osc = this.ctx.createOscillator();
-      osc.type = 'sine';
-      osc.connect(gain);
-      osc.start();
-      nodes.osc = osc;
-    } else if (track.constructor.name === 'BinauralBeatTrack') {
-      // Binaural requires stereo separation
-      const merger = this.ctx.createChannelMerger(2);
-      merger.connect(gain);
-
-      const leftOsc = this.ctx.createOscillator();
-      const rightOsc = this.ctx.createOscillator();
-      const leftPan = this.ctx.createStereoPanner();
-      const rightPan = this.ctx.createStereoPanner();
-
-      leftPan.pan.value = -1;
-      rightPan.pan.value = 1;
-
-      leftOsc.connect(leftPan).connect(merger, 0, 0);
-      rightOsc.connect(rightPan).connect(merger, 0, 1);
-
-      leftOsc.start();
-      rightOsc.start();
-
-      nodes.leftOsc = leftOsc;
-      nodes.rightOsc = rightOsc;
-    } else if (track.constructor.name === 'IsochronicTrack') {
-      // Isochronic: Tone -> Pulse Gain -> Output
-      const pulseGain = this.ctx.createGain();
-      pulseGain.gain.value = 0.5; // Base gain for modulation
-      pulseGain.connect(gain);
-
-      const osc = this.ctx.createOscillator();
-      osc.type = 'sine'; // Carrier
-      osc.connect(pulseGain);
-      osc.start();
-
-      // LFO for pulsing (Square wave for sharp on/off)
-      const lfo = this.ctx.createOscillator();
-      lfo.type = 'square';
-
-      // LFO Gain to scale modulation depth (0 to 1 range)
-      const lfoGain = this.ctx.createGain();
-      lfoGain.gain.value = 0.5;
-
-      lfo.connect(lfoGain).connect(pulseGain.gain);
-      lfo.start();
-
-      nodes.osc = osc;
-      nodes.lfo = lfo;
-      nodes.pulseGain = pulseGain;
-    }
-
-    this.nodes.set(track.id, nodes);
-  }
-
-  _updateTrackParams(track, time) {
-    const nodes = this.nodes.get(track.id);
-    if (!nodes) return;
-
-    // Generic Gain
-    const gainVal = track.getParameter('gain')?.getValue(time) ?? 0.5;
-    nodes.output.gain.setTargetAtTime(gainVal, this.ctx.currentTime, 0.05);
-
-    if (track.constructor.name === 'SineTrack') {
-      const freq = track.getParameter('frequency')?.getValue(time) ?? 440;
-      nodes.osc.frequency.setTargetAtTime(freq, this.ctx.currentTime, 0.05);
-    } else if (track.constructor.name === 'BinauralBeatTrack') {
-      const carrier = track.getParameter('carrier')?.getValue(time) ?? 200;
-      const beat = track.getParameter('beat')?.getValue(time) ?? 10;
-
-      nodes.leftOsc.frequency.setTargetAtTime(carrier, this.ctx.currentTime, 0.05);
-      nodes.rightOsc.frequency.setTargetAtTime(carrier + beat, this.ctx.currentTime, 0.05);
-    } else if (track.constructor.name === 'IsochronicTrack') {
-      const freq = track.getParameter('frequency')?.getValue(time) ?? 200;
-      const rate = track.getParameter('pulseRate')?.getValue(time) ?? 10;
-      // Duty cycle not fully implemented in this simple LFO model, defaulting to 50%
-
-      nodes.osc.frequency.setTargetAtTime(freq, this.ctx.currentTime, 0.05);
-      nodes.lfo.frequency.setTargetAtTime(rate, this.ctx.currentTime, 0.05);
-    }
-  }
 }
